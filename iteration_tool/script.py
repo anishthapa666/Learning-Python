07796b6aaaeef7c026266ebc71e5aa0d8e2074bb
anishthapa66 <anishthapa6699@gmail.com>
2024-12-30 19:41:03 +0545

    simple calculator

--- iteration_tool/script.py
+++ iteration_tool/script.py
@@ -1,3 +1,13 @@
-def sum(a,b ):
-    return a +b
-print(f"sum is {sum(6,7)}")
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+
+operation = input("Enter the operation to be done: ")
+
+if operation == "+":
+    print(num1 + num2)
+elif operation == "*":
+    print(num1*num2)
+elif operation == "-":
+    print(num1-num2)
+elif operation == "/":
+    print(num1//num2)
